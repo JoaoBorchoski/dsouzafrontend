2b191d08c3610c6bb2e6a18bbf63f666fe2b8887
Joao Borchoski <joaoborchoskidev@gmail.com>
2023-09-28 19:17:50 -0300

    fix axios

--- src/services/axios.js
+++ src/services/axios.js
@@ -1,7 +1,8 @@
 import axios from "axios";
 
 export const api = axios.create({
-    baseURL: "http://localhost:3001",
+    baseURL: "https://dsouza.onrender.com",
 });
 
 //https://dsouza.onrender.com
+//http://localhost:3001
